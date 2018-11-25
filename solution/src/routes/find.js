e75be4c092f9e98ab541a8a4420819a76ac58de4
Matt Davis <matthewpeterdavis@gmail.com>
2018-11-25 15:13:11 +0200

    change solution for find.js
    - add explaining comments
    - change error responses depending on the error
      - check for correct qs parameter
      - check against a whitelist
      - check readFile doesn't error

--- solution/src/routes/find.js
+++ solution/src/routes/find.js
@@ -1,38 +1,42 @@
+// import path & url & fs & querystring
 const path = require('path')
 const fs = require('fs')
 const url = require('url')
-const querystring = require('querystring')
+const qs = require('querystring')
 
-const returnBear = ((request, response, bear) => {
-  const imgPath = path.join(__dirname,'..', 'woods', bear)
-  const image = fs.readFileSync(imgPath)
-  response.writeHead(200, {'Content-type':'image/jpg'})
-  return response.end(image)
-})
 
 module.exports = ((request, response) => {
+
+  // Object deconstruction to get keys and values 
   const { query } = url.parse(request.url)
-  const { bear } = querystring.parse(query)
-  
+  const { bear } = qs.parse(query)
+
+  // Check query exists
   if (!bear) {
-    response.writeHead(400)
-    return response.end('Search criteria cannot be found!')
+    response.writeHead(400, { 'Content-Type': 'text/plain' })
+    response.end('`bear` parameter required')
+    return
   }
-  switch (bear){
-    case `one`:
-      return returnBear(request, response, 'bear_one.jpg')
 
-    case `two`:
-      return returnBear(request, response, 'bear_two.jpg')
+  // Check the query matches one of our files
+  const bearWhiteList = ['one', 'two', 'three', 'four']
 
-    case `three`:
-    return returnBear(request, response, 'bear_three.jpg')
-
-    case `four`:
-      return returnBear(request, response, 'bear_four.jpg')
-
-    default:
-      response.writeHead(400)
-      return response.end('Search criteria cannot be found!')
+  if (!bearWhiteList.includes(bear)) {
+    response.writeHead(404, { 'Content-Type': 'text/plain' })
+    response.end('No bear found!')
+    return
   }
+
+  // Read the file asynchronously, handle a file read error or send the file
+  const fileName = `bear_${bear}.jpg`
+  const filePath = path.join(__dirname, '..', 'woods', fileName)
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      response.writeHead(500, { 'Content-Type': 'text/plain' })
+      response.end('Sorry, server done broke')
+      return
+    }
+    response.writeHead(200, { 'Content-Type': 'image/jpeg' })
+    response.end(file)
+  })
 })
